4edd68cb78b9d9f0ff9aae5ec38d073986ff6b88
AnatolySSS <45735855+AnatolySSS@users.noreply.github.com>
2018-12-16 00:21:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,24 +4,34 @@ var count = 0;
 
 function jump() {
 
+  jumpTop();
+  jumpLeft();
+  count++;
+  document.getElementById('number').innerHTML=count;
+}
+
+function jumpTop() {
+  function getRandomInRangeTop(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  top = getRandomInRangeTop(5, 645) + "px";
+  document.getElementById('circle').style.top = top;
+}
+
+function jumpLeft() {
   function getRandomInRangeLeft(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   left = getRandomInRangeLeft(5, 345) + "px";
-
-  function getRandomInRangeTop(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  top = getRandomInRangeTop(5, 600) + "px";
-
-  document.getElementById('circle').style.top = top;
   document.getElementById('circle').style.left = left;
-
-  count++;
-  document.getElementById('number').innerHTML=count;
 }
+
+
+
+
+
 
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
